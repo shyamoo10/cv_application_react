087da1d465edab06d88980f8808a76a94a920eec
shyam <rajshyam151@gmail.com>
2024-03-30 12:07:51 +0530

    general information form added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import General_form from "./general_info";
+import  "../src/index.css"
+
+
+function App() {
+  return (
+    <div className="App">
+      <General_form/>
+    </div>
+  );
+}
+
+export default App;
