b9f359a72284abb0f9b2b4703a382c36f022eef8
shyam <rajshyam151@gmail.com>
2024-03-30 14:51:41 +0530

    edit button logic added

--- src/general_info.js
+++ src/general_info.js
@@ -5,14 +5,20 @@ const General_form=()=>{
   const[email,setemail]=useState('')
   const[number,setnumber]=useState('')
   const[isSubmit,setisSubmit]=useState(false)
+  const[isedit,setisedit]=useState(false)
   const handleSubmit=(e)=>{
      e.preventDefault()
      setisSubmit(true)
+     setisedit(false)
   }
+   const handleEdit=()=>{
+       setisedit(true)
+       setisSubmit(false)
+   }
     return(
         <div className="general_info">
-         <h2>General Informations</h2>
-         <form  onSubmit={handleSubmit}>
+          <h2>General Informations</h2>
+         {  (!isSubmit || isedit) && <form  onSubmit={handleSubmit}>
             <label>Name:</label>
             <input type="text" value={Name}  onChange={(e)=>setname(e.target.value)}></input>
             <label>Email:</label>
@@ -20,12 +26,13 @@ const General_form=()=>{
             <label>Phone No:</label>
             <input  type="text"  value={number}  onChange={(e)=> setnumber(e.target.value)}></input>
             <button>Submit</button>
-         </form>
+         </form> }
          {isSubmit && (
             <div>
                 <p>{Name}</p>
                 <p>{email}</p>
                 <p>{number}</p>
+                <button  onClick={handleEdit}>Edit</button>
                 </div>
          )}
       </div>
